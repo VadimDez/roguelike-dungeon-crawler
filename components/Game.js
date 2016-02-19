c592d3b37e9d119c2f0f02dbea60c631679585dc
Vadym Yatsyuk <vasp3d@gmail.com>
2016-02-19 12:43:38 +0100

    added game component

---
+++ components/Game.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Vadym Yatsyuk on 19/02/16
+ */
+
+import React from 'react';
+class Game extends React.Component {
+  render() {
+    return (
+      <div>Gane</div>
+    )
+  }
+}
+
+export default Game
--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 /**
  * Created by Vadym Yatsyuk on 18/02/16
  */
+import React from 'react';
+import {render} from 'react-dom';
+
 import './styles/main.scss';
+import Game from './components/Game';
+
+render(
+  <Game />,
+  document.getElementById('app')
+)
