e51bd0cf5bee0b8db58c2eb653f3a602114af405
Vadym Yatsyuk <vasp3d@gmail.com>
2016-03-04 12:38:12 +0100

    changed map, now it's possible to win

--- maps/map2.js
+++ maps/map2.js
@@ -2,11 +2,16 @@
  * Created by vadimdez on 01/03/16.
  */
 import Enemy from './../Entities/Enemy'
+import Health from './../Entities/Health'
+import Weapon from './../Entities/Weapon'
 import {getRandomEmptyPointOnMap} from './../utils'
 
 export default function () {
   let objects = [
-    new Enemy(4, 100, true)
+    new Enemy(4, 100, true),
+    new Health(50),
+    new Health(50),
+    new Weapon('Sword', 20)
   ]
   return function () {
     let randomPoint
@@ -16,11 +21,18 @@ export default function () {
         y: 1
       },
       map: [
-        [1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 1],
-        [1, 0, 0, 0, 1],
-        [1, 0, 0, 0, 1],
-        [1, 1, 1, 1, 1]
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
       ]
     }
 
