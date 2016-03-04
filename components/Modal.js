1ea16748a532d7c1d9e593fb34171899ec1b4c27
Vadym Yatsyuk <vasp3d@gmail.com>
2016-03-04 12:49:31 +0100

    changed modal position

--- components/Modal.js
+++ components/Modal.js
@@ -7,9 +7,11 @@ import React from 'react'
 export default class Modal extends React.Component {
   render() {
     return (
-      <div>
-        <h3>{this.props.text}</h3>
-        <button onClick={this.props.onClick}>Ok</button>
+      <div className="modal-container">
+        <div className="modal">
+          <h3>{this.props.text}</h3>
+          <button onClick={this.props.onClick}>Ok</button>
+        </div>
       </div>
     )
   }
